5420293a15c8e26ca76d638c6b5bfa22615f0353
Alex Almanza <44lmanza@gmail.com>
2022-05-21 10:54:54 -0700

    Setup AlertContext

---
+++ src/context/alert/AlertContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import alertReducer from './AlertReducer';
+
+export const AlertContext = createContext();
+AlertContext.displayName = 'AlertContext';
+
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    });
+
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }, 3000));
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+AlertContext.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AlertContext;
